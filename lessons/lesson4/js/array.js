407f45d9f3ef19344e71a90ab725e504f3688069
jestyal <natali.doroshenko14gmail.com>
2021-09-27 16:25:23 +0500

    Add lesson 4

---
+++ lessons/lesson4/js/array.js
@@ -0,0 +1,22 @@
+function sumInput() {
+    let numbers = [];
+
+    while (true) {
+        let value = prompt("Введите число:", 0);
+        if (value === null || value === "" || !isFinite(value)) break;
+        numbers.push(+value);
+    }
+
+    //сумма элементов массива
+    let sum = 0;
+    for (let number of numbers) {
+        sum += number;
+    }
+
+    //сортировка по возрастанию
+    let sort = numbers.sort(function compareNumbers(a, b) {
+        return a - b;
+    });
+
+    return `Сумма: ${sum}\nМассив: ${sort}`;
+}
