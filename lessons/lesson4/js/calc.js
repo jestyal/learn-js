acad0fcf7e401e4c10907469518c186b2d366a69
jestyal <natali.doroshenko14gmail.com>
2021-09-28 20:16:34 +0500

    Add script to calculator auto (not full)

--- lessons/lesson4/js/calc.js
+++ lessons/lesson4/js/calc.js
@@ -1,14 +1,3 @@
-const form = document.querySelector('#calculate-form');
-const resultText = +document.querySelector('#result span');
-
-//все значения инпутов и селектов
-const auctionPrice = +document.querySelector('#calculator_raw_cost').value,
-    engineVolume = +document.querySelector('#calculator_volume').value,
-    enginePower = +document.querySelector('#calculator_power').value;
-
-//все радио кнопки
-const radioFuelType = document.getElementsByName('calculator[fueltype]');
-
 //все поля с ошибками
 const msgErrorAuctionPrice = document.querySelector('#msg-error_raw_cost'),
     msgErrorAuction = document.querySelector('#msg-error_auction'),
@@ -24,11 +13,23 @@ allErrors.forEach(function(val) {
 });
 
 
+
+
 //блок с аукционом и типом услуг
 const auction = document.querySelector('#calculator_auction');
 
 const servicesBlock = document.querySelector("#services");
 servicesBlock.style.display = "none";
+
+
+// const a = {
+//     "auction1": {
+//         "Целый автомобиль": 100,
+//     },
+//     "auction2": {
+//         "Распил рамный": 100,
+//     },
+// }
 
 auction.addEventListener("change", (event) => {
     const servicesContainer = document.querySelector("#calculator_service_type");
@@ -38,15 +39,6 @@ auction.addEventListener("change", (event) => {
     //создаю блок с типом услуг
     let auctionValues = [];
     servicesContainer.innerHTML = "";
-
-    function createServices(auctionValues) {
-        for (let i = 0; i < auctionValues.length; i++) {
-            let serviceTypeElement = document.createElement("div");
-            serviceTypeElement.classList.add("form__radio");
-            serviceTypeElement.innerHTML = '<label class="form__label"><input type="radio" class="input__radio" name="service_type" /> ' + auctionValues[i] + '</label>';
-            servicesContainer.append(serviceTypeElement);
-        }
-    }
 
     // в зависимости от выбранного аукциона, показываем тот или иной список услуг
     const auctionType = auction.value;
@@ -60,6 +52,39 @@ auction.addEventListener("change", (event) => {
         createServices([...services2]);
     }
 
+    function createServices(auctionValues) {
+        for (let i = 0; i < auctionValues.length; i++) {
+            let serviceTypeElement = document.createElement("div");
+            serviceTypeElement.classList.add("form__radio");
+            serviceTypeElement.innerHTML = '<label class="form__label"><input type="radio" class="input__radio" name="service_type" value="' + [i] + '" /> ' + auctionValues[i] + '</label>';
+            servicesContainer.append(serviceTypeElement);
+
+
+            let radioServiceType = serviceTypeElement.querySelector("input");
+            radioServiceType.addEventListener("change", (event) => {
+                const serviceTypeValue = radioServiceType.value;
+
+                //прописываю цену конкретной услуге
+                let serviceCost = 0;
+                if(auctionType == 1 && serviceTypeValue == 0) {
+                    serviceCost = 100;
+                } else if(auctionType == 1 && serviceTypeValue == 1) {
+                    serviceCost = 200;
+                } else if(auctionType == 1 && serviceTypeValue == 2) {
+                    serviceCost = 300;
+                } else if(auctionType == 2 && serviceTypeValue == 0) {
+                    serviceCost = 400;
+                } else if(auctionType == 2 && serviceTypeValue == 1) {
+                    serviceCost = 500;
+                } else {
+                    serviceCost = 1;
+                }
+
+                console.log(serviceCost)
+                // return serviceCost;
+            });
+        }
+    }
 })
 
 
@@ -69,8 +94,12 @@ engineVolumeBlock.style.display = "none";
 const enginePowerBlock = document.querySelector("#engine_power");
 enginePowerBlock.style.display = "none";
 
+
+
 const fuelTypeBlock = document.querySelector('#calculator_fuel_type');
 const fuelTypes = ['бензин (включая гибрид)', 'дизельное топливо', 'электрический'];
+
+
 
 function createFuelTypes () {
     for (let i = 0; i < fuelTypes.length; i++) {
@@ -89,6 +118,23 @@ function createFuelTypes () {
             } else if(fuelValue == 2) {
                 enginePowerBlock.style.display = "block";
             }
+
+
+            //прописываю цену конкретному типу топлива
+            let fuelCost = 0;
+            if(fuelValue == 0) {
+                fuelCost = 2000;
+            } else if(fuelValue == 1) {
+                fuelCost = 3000;
+            } else if(fuelValue == 2) {
+                fuelCost = 1000;
+            } else {
+                fuelCost = 1;
+            }
+            console.log(fuelCost)
+            // return fuelCost;
+
+
         })
 
         fuelTypeBlock.append(fuelTypeElement);
@@ -98,14 +144,29 @@ createFuelTypes ();
 
 
 
-
-
+//подсчет цены
+const form = document.querySelector('#calculate-form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+
+    let totalCost = 0;
+    let resultText = document.querySelector('#result span');
+
+    let auctionPriceValue = +document.querySelector('#calculator_raw_cost').value,
+        engineVolumeValue = +document.querySelector("#engine_volume input").value,
+        enginePowerValue = +document.querySelector("#engine_power input").value;
+
+
+    totalCost = auctionPriceValue + engineVolumeValue + enginePowerValue;
+    resultText.innerHTML = totalCost;
+    console.log(totalCost);
 })
 
-function calcTotalCost() {
-    // let totalCost;
-    // resultText.textContent = totalCost;
-}
+
+
+
+
+
+
+
