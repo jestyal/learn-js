5a6434f1ad7b2aa850ecc99de65140f575c83941
jestyal <natali.doroshenko14gmail.com>
2021-10-11 22:44:47 +0500

    Fixed chat structure and add chat script

--- lessons/lesson6/js/chat.js
+++ lessons/lesson6/js/chat.js
@@ -1,25 +1,76 @@
-let input = document.querySelector("#input");
-let button = document.querySelector("#send-btn");
+let userName = document.querySelector("#userName");
+let comment = document.querySelector("#comment");
+let sendBtn = document.querySelector("#send-btn");
+let resetBtn = document.querySelector("#reset-btn");
 let commentsBlock = document.querySelector(".comments__wrap");
+let avatarInput = document.querySelector("#avatar");
+let avatarBlock = document.querySelector(".avatar__wrap");
 
-let reg = /viagra|XXX/gmi;
+// let reg = /viagra|XXX/gmi;
+//
+// function createElement(elementValue) {
+//     let item = document.createElement("div");
+//     item.classList.add("comment__item");
+//     item.innerHTML = elementValue;
+//     commentsBlock.append(item);
+// }
+//
+//
+// button.addEventListener("click", () => {
+//     let value = input.value;
+//
+//     if (reg.test(value)) {
+//         let result = value.replace(reg, "***");
+//         // commentsBlock.textContent = result;
+//         createElement(result);
+//     } else {
+//         createElement(value);
+//     }
+// });
 
-function createElement(elementValue) {
-    let item = document.createElement("div");
-    item.classList.add("comment__item");
-    item.innerHTML = elementValue;
-    commentsBlock.append(item);
+//новое
+
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    saveToLocalStorage();
+
+    let textAreaValue = comment.value;
+    commentsBlock.innerHTML += userName.value + ": " + textAreaValue + "<br>";
+    // function createElement(elementValue) {
+    //     let item = document.createElement("div");
+    //     item.classList.add("comment__item");
+    //     item.innerHTML = elementValue;
+    //     commentsBlock.append(item);
+    // }
+    // commentsBlock.textContent += textAreaValue;
+    // commentsBlock.innerHTML += "<br>";
+    comment.value = "";
+    comment.focus();
+
+    // localStorage.setItem('comments', document.querySelector('#container').innerHTML);
+    // или
+    // let localComments = localStorage.setItem('comments');
+    // localStorage.setItem('comments', localComments + "<br>" + comment);
+
+
+    let avatarFileName = avatarInput.files[0].name;
+    // avatarBlock.innerHTML += `<img src="img/${avatarFileName}" />`;
+});
+
+document.querySelector('form').addEventListener('reset', (event) => {
+    event.preventDefault();
+    readFromLocalStorage();
+});
+
+function saveToLocalStorage() {
+    let value = userName.value;
+    localStorage.setItem('username', value);
 }
 
+function readFromLocalStorage() {
+    let value = localStorage.getItem('username');
+    userName.value = value;
+}
 
-button.addEventListener("click", () => {
-    let value = input.value;
+readFromLocalStorage();
 
-    if (reg.test(value)) {
-        let result = value.replace(reg, "***");
-        // commentsBlock.textContent = result;
-        createElement(result);
-    } else {
-        createElement(value);
-    }
-});
--- lessons/lesson6/js/form.js
+++ lessons/lesson6/js/form.js
@@ -1,43 +1,101 @@
-let errorMsg = document.getElementById('msg-error');
-let successMsg = document.getElementById('msg-success');
 
-let name = document.getElementById('name'),
-    phone = document.getElementById('phone'),
-    email = document.getElementById('email'),
-    birthday = document.getElementById('birthday'),
-    password = document.getElementById('password'),
-    policyCheckbox = document.getElementById('policy-check');
+let successMsg = document.querySelector('#msg-success');
+let errorMsg = document.querySelector('#msg-error');
 
-function validateInput() {
-    errorMsg.innerHTML = '';
-    successMsg.innerHTML = '';
+let name = document.querySelector('#name'),
+    phone = document.querySelector('#phone'),
+    email = document.querySelector('#email'),
+    birthday = document.querySelector('#birthday'),
+    password = document.querySelector('#password'),
+    policyCheckbox = document.querySelector('#policy-check');
 
-    if(name.value == '') {
-        errorMsg.innerHTML += `Ваше имя не заполнено<br>`;
+// function validateInput() {
+//     errorMsg.innerHTML = '';
+//     successMsg.innerHTML = '';
+//
+//     if(name.value == '') {
+//         errorMsg.innerHTML += `Ваше имя не заполнено<br>`;
+//     }
+//
+//     if(phone.value == '') {
+//         errorMsg.innerHTML += `Ваш телефон не заполнен<br>`;
+//     }
+//
+//     if(email.value == '') {
+//         errorMsg.innerHTML += `Ваш email не заполнен<br>`;
+//     }
+//
+//     if(birthday.value == '') {
+//         errorMsg.innerHTML += `Ваш день рождения не заполнен<br>`;
+//     }
+//
+//     if(password.value.length <= 7) {
+//         errorMsg.innerHTML += `Ваш пароль слишком короткий<br>`;
+//     }
+//
+//     if(!policyCheckbox.checked) {
+//         errorMsg.innerHTML += `Примите политику использования данных<br>`;
+//     }
+//
+//     if(name.value != '' && phone.value != '' && email.value != '' && birthday.value != '' && password.value != '' && policyCheckbox.checked) {
+//         successMsg.innerHTML += `Добро пожаловать, ${name.value}!`;
+//     }
+//
+// }
+
+//Проверка для одного поля
+let errors = [];
+function checkValidity(input) {
+    let validity = input.validity;
+
+    if (validity.patternMismatch) {
+        errors.push('Неверный формат заполнения');
     }
 
-    if(phone.value == '') {
-        errorMsg.innerHTML += `Ваш телефон не заполнен<br>`;
+    if (validity.rangeOverflow) {
+        let max = input.max;
+        errors.push('Максимальное значение не может быть больше чем ' + max);
     }
 
-    if(email.value == '') {
-        errorMsg.innerHTML += `Ваш email не заполнен<br>`;
+    if (validity.rangeUnderflow) {
+        let min = input.min;
+        errors.push('Минимальное значение не может быть больше чем ' + min);
     }
 
-    if(birthday.value == '') {
-        errorMsg.innerHTML += `Ваш день рождения не заполнен<br>`;
+    // И остальные проверки валидности...
+}
+
+//Проверка для всех полей
+function checkAll() {
+    //получаем все инпуты
+    let inputs = document.querySelectorAll("input");
+
+    //перебираем их и на каждый вызываем функцию валидации
+    for (let input of inputs) {
+        checkValidity(input);
     }
 
-    if(password.value.length <= 7) {
-        errorMsg.innerHTML += `Ваш пароль слишком короткий<br>`;
-    }
+    //выводим ошибки в div
 
-    if(!policyCheckbox.checked) {
-        errorMsg.innerHTML += `Примите политику использования данных<br>`;
-    }
+    errorMsg.innerHTML = errors.join('. \n');
+}
 
-    if(name.value != '' && phone.value != '' && email.value != '' && birthday.value != '' && password.value != '' && policyCheckbox.checked) {
-        successMsg.innerHTML += `Добро пожаловать, ${name.value}!`;
-    }
 
+function validateEmail(email) {
+    let mailFormat = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/gm;
+
+    // let reg = /java|котики|hello/gim;
+    // if (reg.test(value)) {
+    //     let result = value.replace(reg, "XXXX");
+    //     resultNode.textContent = result;
+    // } else {
+    //     resultNode.textContent = "Да!";
+    // }
+    if (email.value.match(mailFormat)) {
+        return true;
+    }
+    else {
+        errorMsg.textContent = "Ваш адрес электронной почты введен неверно!";
+        return false;
+    }
 }
