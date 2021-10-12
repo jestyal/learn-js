00c16dda29a829936433dd62f841a1fe99cad2a4
jestyal <natali.doroshenko14gmail.com>
2021-10-12 15:07:34 +0500

    add chat script

--- lessons/lesson6/js/chat.js
+++ lessons/lesson6/js/chat.js
@@ -1,65 +1,35 @@
 let userName = document.querySelector("#userName");
 let comment = document.querySelector("#comment");
-let sendBtn = document.querySelector("#send-btn");
-let resetBtn = document.querySelector("#reset-btn");
+// let sendBtn = document.querySelector("#send-btn");
+// let resetBtn = document.querySelector("#reset-btn");
 let commentsBlock = document.querySelector(".comments__wrap");
 let avatarInput = document.querySelector("#avatar");
-let avatarBlock = document.querySelector(".avatar__wrap");
-
-// let reg = /viagra|XXX/gmi;
-//
-// function createElement(elementValue) {
-//     let item = document.createElement("div");
-//     item.classList.add("comment__item");
-//     item.innerHTML = elementValue;
-//     commentsBlock.append(item);
-// }
-//
-//
-// button.addEventListener("click", () => {
-//     let value = input.value;
-//
-//     if (reg.test(value)) {
-//         let result = value.replace(reg, "***");
-//         // commentsBlock.textContent = result;
-//         createElement(result);
-//     } else {
-//         createElement(value);
-//     }
-// });
-
-//новое
+let errorMsg = document.querySelector(".msg-error");
 
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
     saveToLocalStorage();
 
+    errorMsg.innerHTML = "";
+
+    let avatarFileName = avatarInput.files[0].name;
     let textAreaValue = comment.value;
-    commentsBlock.innerHTML += userName.value + ": " + textAreaValue + "<br>";
-    // function createElement(elementValue) {
-    //     let item = document.createElement("div");
-    //     item.classList.add("comment__item");
-    //     item.innerHTML = elementValue;
-    //     commentsBlock.append(item);
-    // }
-    // commentsBlock.textContent += textAreaValue;
-    // commentsBlock.innerHTML += "<br>";
+    if(textAreaValue.length != 0) {
+        commentsBlock.innerHTML += `<div class="avatar__wrap"><img class="avatar" src="img/${avatarFileName}" alt="" /></div>` + userName.value + ": " + textAreaValue + "<br>";
+    } else {
+        errorMsg.textContent += 'Введите комментарий';
+    }
     comment.value = "";
     comment.focus();
 
-    // localStorage.setItem('comments', document.querySelector('#container').innerHTML);
-    // или
-    // let localComments = localStorage.setItem('comments');
-    // localStorage.setItem('comments', localComments + "<br>" + comment);
-
-
-    let avatarFileName = avatarInput.files[0].name;
-    // avatarBlock.innerHTML += `<img src="img/${avatarFileName}" />`;
+    localStorage.setItem('comments', commentsBlock.innerHTML);
 });
 
 document.querySelector('form').addEventListener('reset', (event) => {
     event.preventDefault();
     readFromLocalStorage();
+
+    localStorage.removeItem('comments');
 });
 
 function saveToLocalStorage() {
