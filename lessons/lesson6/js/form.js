87a8cadf005ff9ffb7f35e942b7046ed4c0f93d5
jestyal <natali.doroshenko14gmail.com>
2021-10-12 22:29:37 +0500

    add form script

--- lessons/lesson6/js/form.js
+++ lessons/lesson6/js/form.js
@@ -1,68 +1,40 @@
-
-let successMsg = document.querySelector('#msg-success');
 let errorMsg = document.querySelector('#msg-error');
 
-let name = document.querySelector('#name'),
-    phone = document.querySelector('#phone'),
-    email = document.querySelector('#email'),
-    birthday = document.querySelector('#birthday'),
-    password = document.querySelector('#password'),
-    policyCheckbox = document.querySelector('#policy-check');
-
-// function validateInput() {
-//     errorMsg.innerHTML = '';
-//     successMsg.innerHTML = '';
-//
-//     if(name.value == '') {
-//         errorMsg.innerHTML += `Ваше имя не заполнено<br>`;
-//     }
-//
-//     if(phone.value == '') {
-//         errorMsg.innerHTML += `Ваш телефон не заполнен<br>`;
-//     }
-//
-//     if(email.value == '') {
-//         errorMsg.innerHTML += `Ваш email не заполнен<br>`;
-//     }
-//
-//     if(birthday.value == '') {
-//         errorMsg.innerHTML += `Ваш день рождения не заполнен<br>`;
-//     }
-//
-//     if(password.value.length <= 7) {
-//         errorMsg.innerHTML += `Ваш пароль слишком короткий<br>`;
-//     }
-//
-//     if(!policyCheckbox.checked) {
-//         errorMsg.innerHTML += `Примите политику использования данных<br>`;
-//     }
-//
-//     if(name.value != '' && phone.value != '' && email.value != '' && birthday.value != '' && password.value != '' && policyCheckbox.checked) {
-//         successMsg.innerHTML += `Добро пожаловать, ${name.value}!`;
-//     }
-//
-// }
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    errors = [];
+    // chekNameInput();
+    // chekPhoneInput();
+    // chekEmailInput();
+    checkAll();
+});
 
 //Проверка для одного поля
 let errors = [];
+
 function checkValidity(input) {
+
     let validity = input.validity;
+
+    if (validity.valueMissing) {
+        let min = input.min;
+        errors.push('Поле ' + input.placeholder + ' не заполнено');
+    }
 
     if (validity.patternMismatch) {
         errors.push('Неверный формат заполнения');
     }
 
     if (validity.rangeOverflow) {
-        let max = input.max;
+        let max = input.maxLength;
         errors.push('Максимальное значение не может быть больше чем ' + max);
     }
 
     if (validity.rangeUnderflow) {
-        let min = input.min;
+        let min = input.minLength;
         errors.push('Минимальное значение не может быть больше чем ' + min);
     }
 
-    // И остальные проверки валидности...
 }
 
 //Проверка для всех полей
@@ -76,26 +48,46 @@ function checkAll() {
     }
 
     //выводим ошибки в div
-
-    errorMsg.innerHTML = errors.join('. \n');
+    errorMsg.innerHTML = errors.join('<br>');
 }
 
+// function chekNameInput() {
+//     let reg = /[А-ЯЁ][а-яё]+\s+[А-ЯЁ][а-яё]+(?:\s+[А-ЯЁ][а-яё]+)?/g;
+//
+//     let name = document.querySelector('#name');
+//     if (name.value.match(reg)) {
+//         return true;
+//     }
+//     else {
+//         errorMsg.textContent = "ФИО введено некорректно";
+//         return false;
+//     }
+// }
+//
+// function chekEmailInput() {
+//     let reg = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/gm;
+//
+//     let email = document.querySelector('#email');
+//     if (email.value.match(reg)) {
+//         return true;
+//     }
+//     else {
+//         errorMsg.textContent = "Электронная почта введена некорректно";
+//         return false;
+//     }
+// }
+//
+// function chekPhoneInput() {
+//     let reg = /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g;
+//
+//     let phone = document.querySelector('#phone');
+//     if (phone.value.match(reg)) {
+//         return true;
+//     }
+//     else {
+//         errorMsg.textContent = "Телефон введен некорректно";
+//         return false;
+//     }
+// }
+//
 
-function validateEmail(email) {
-    let mailFormat = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/gm;
-
-    // let reg = /java|котики|hello/gim;
-    // if (reg.test(value)) {
-    //     let result = value.replace(reg, "XXXX");
-    //     resultNode.textContent = result;
-    // } else {
-    //     resultNode.textContent = "Да!";
-    // }
-    if (email.value.match(mailFormat)) {
-        return true;
-    }
-    else {
-        errorMsg.textContent = "Ваш адрес электронной почты введен неверно!";
-        return false;
-    }
-}
---
+++ lessons/lesson6/js/notes.js
@@ -0,0 +1,18 @@
+let date = document.querySelector("#date");
+let noteTitle = document.querySelector("#noteTitle");
+let comment = document.querySelector("#comment");
+
+
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    saveToLocalStorage();
+});
+
+function saveToLocalStorage() {
+    let value = date.value;
+    let title = noteTitle.value;
+    let noteText = comment.value;
+    localStorage.setItem('date', value);
+    localStorage.setItem('title', title);
+    localStorage.setItem('title', noteText);
+}
