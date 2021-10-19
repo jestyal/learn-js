28e64feca0d9882163391d8271234d7cc1782d09
jestyal <natali.doroshenko14gmail.com>
2021-10-19 13:43:56 +0500

    Add script to form.js

--- lessons/lesson7/js/form.js
+++ lessons/lesson7/js/form.js
@@ -1,89 +1,60 @@
-let errorMsg = document.querySelector('#msg-error');
-
-document.querySelector('form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    errors = [];
-    // chekNameInput();
-    // chekPhoneInput();
-    // chekEmailInput();
-    checkAll();
-});
-
-//Проверка для одного поля
-let errors = [];
-
-function checkValidity(input) {
-
-    let validity = input.validity;
-
-    if (validity.valueMissing) {
-        let min = input.min;
-        errors.push('Поле ' + input.placeholder + ' не заполнено');
+class Validator {
+    isEmail(email) {
+        let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return reg.test(String(email));
     }
 
-    if (validity.patternMismatch) {
-        errors.push('Неверный формат заполнения');
+    isDomain(domain) {
+        let reg = /^(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/;
+        return reg.test(String(domain));
     }
 
-    if (validity.rangeOverflow) {
-        let max = input.maxLength;
-        errors.push('Максимальное значение не может быть больше чем ' + max);
+    isDate(date) {
+        let reg = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+        return reg.test(String(date));
     }
 
-    if (validity.rangeUnderflow) {
-        let min = input.minLength;
-        errors.push('Минимальное значение не может быть больше чем ' + min);
+    isPhone(phone) {
+        let reg = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+        return reg.test(String(phone));
+    }
+}
+
+class ValidatorStatic {
+    static isEmail(email) {
+        let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return reg.test(String(email));
+    }
+
+    static isDomain(domain) {
+        let reg = /^(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/;
+        return reg.test(String(domain));
+    }
+
+    static isDate(date) {
+        let reg = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+        return reg.test(String(date));
+    }
+
+    static isPhone(phone) {
+        let reg = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+        return reg.test(String(phone));
     }
 
 }
 
-//Проверка для всех полей
-function checkAll() {
-    //получаем все инпуты
-    let inputs = document.querySelectorAll("input");
 
-    //перебираем их и на каждый вызываем функцию валидации
-    for (let input of inputs) {
-        checkValidity(input);
-    }
+//стандартная вызывается вот так
+let validator = new Validator();
+console.log(validator.isEmail('alisa@mail.ru'));
+console.log(validator.isDomain('itgirlschool.ru'));
+console.log(validator.isDate('12.05.2021'));
+console.log(validator.isPhone('+7(910)123-45-67')); //тут используем формат своей страны
 
-    //выводим ошибки в div
-    errorMsg.innerHTML = errors.join('<br>');
-}
+//а статическая так
+console.log(ValidatorStatic.isEmail('alisa@mail.ru'));
+console.log(ValidatorStatic.isDomain('itgirlschool.ru'));
+console.log(ValidatorStatic.isDate('12.05.2021'));
+console.log(ValidatorStatic.isPhone('+7(910)123-45-67')); //тут используем формат своей страны
 
 
-//
-//
-// class Calculator {
-//     static sum (a, b) {
-//         return a + b;
-//     }
-//
-//     static minus (a, b) {
-//         return a - b;
-//     }
-//
-//     static mult (a, b) {
-//         return a * b;
-//     }
-//
-//     static divide (a, b) {
-//         return a / b;
-//     }
-// }
-
-// class Validation {
-//     static сheckEmptyInputs(inputs) {
-//         let isEmpty = false;
-//         for (let input of inputs) {
-//             if (input.value == '')
-//                 isEmpty = true;
-//         }
-//         return isEmpty;
-//     }
-// }
-//
-// if (Validation.сheckEmptyInputs(document.getElementsByClassName('test'))) {
-//     //есть пустой элемент
-//     alert('Есть пустой элемент!');
-// }
