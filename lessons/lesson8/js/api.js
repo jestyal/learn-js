485be85e1e93937301e67068f86c0c97edac26fa
jestyal <natali.doroshenko14gmail.com>
2021-10-27 14:08:34 +0500

    Add script to api

--- lessons/lesson8/js/api.js
+++ lessons/lesson8/js/api.js
@@ -1,14 +1,26 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    getWeather("Moscow");
+    // getWeather("Moscow");
+    getDog();
 })
 
-function getWeather(city) {
-    fetch("https://api.openweathermap.org/data/2.5/.........." + city + "&..........")
+// function getWeather(city) {
+//     fetch("https://api.openweathermap.org/data/2.5/.........." + city + "&..........")
+//         .then(response => response.json())
+//         .then(weather => {
+//             console.log(weather);
+//             document.querySelector(".city").innerText = weather.name;
+//             //
+//         })
+//         .catch(errors => console.log(errors));
+// }
+
+
+function getDog() {
+    fetch("https://dog.ceo/api/breeds/image/random")
         .then(response => response.json())
-        .then(weather => {
-            console.log(weather);
-            document.querySelector(".city").innerText = weather.name;
-            //
+        .then(dog => {
+            document.querySelector(".dog__wrap img").setAttribute("src", dog.message);
         })
         .catch(errors => console.log(errors));
 }
+
